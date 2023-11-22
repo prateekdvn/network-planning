5f3eaa21705fd294a5b36140ee9467434d69a3d6
Dhawan <prateekdhawan@kpmg.com>
2023-11-22 07:04:14 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './image.png';
+import curr from './currency.png';
+import lang from './language.png';
+
+// window.onload = init;
+
+// function init () {
+//   const map = new ol.map ({
+//     view: new ol.View ({
+//       center: [0,0],
+//       zoom: 2
+//     }),
+//     layers: [
+//       new ol.layers.Tile ({
+//         source: new ol.source.OSM()
+//       })
+//     ],
+//     target: 'js-map'
+//   }) 
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='top-bar'>
+        <img src={logo} className='logo' alt='' />
+        <img src={curr} className='curr' alt='' />
+        <img src-={lang} className='lang' alt='' />
+      </div>
     </div>
   );
 }
